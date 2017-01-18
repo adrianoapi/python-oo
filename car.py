5ca1aa9af6628a85cceeb95ac5fa9a5066069210
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-18 14:26:32 -0200

    méttodos

--- car.py
+++ car.py
@@ -1,7 +1,10 @@
 class Car:
     
-    def __init__(self):
-        self.color = 'silver'
-        self.year = 2017
-        self.make = 'wolksvagen'
-        self.name = 'gol'
+    def __init__(self, name):
+        #self.color = color
+        #self.year = year
+        #self.make = make
+        self.name = name
+    
+    def drive(self):
+        print('started ', self.name)
